f9e21cc8692febf589cfe13d5c6cb2381ef29044
emilyduraes <emilyduraes@yahoo.com.br>
2019-07-16 11:45:04 -0300

    começar a lógica do jogo

--- JS/scriptMemoria.js
+++ JS/scriptMemoria.js
@@ -13,6 +13,8 @@ function embaralhar(lista){
 
 let cartas = document.querySelectorAll(".card");
 
+let cartaVirada = null;
+
 let imagensSalvas = ["Bee.png", "blue-boss.png",  "clown.png", "Cuphoof.png", "dice-man.png", "kettle.png", "mugman.png", "the-devil.png"];
 
 let imagens = imagensSalvas.concat(imagensSalvas);
@@ -22,3 +24,27 @@ imagens = embaralhar(imagens);
 for(let i = 0; i < cartas.length; i++){
     cartas[i].style.backgroundImage = `url("images/${imagens[i]}")`;
 }
+
+setTimeout(function(){
+    for(let carta of cartas){
+        carta.style.backgroundImage = 'url("images/Cuphead_and_his_pal_Mugman.png)';
+        carta.onclick = function(){
+
+            if(cartaVirada && cartaVirada.id !== carta.id){
+                if(cartaVirada.style.backgroundImage === carta.style.backgroundImage){
+                    cartaVirada.onclick = null;
+                    carta.onclick = null;
+                }
+                else{
+                    carta.style.backgroundImage = 'url("images/Cuphead_and_his_pal_Mugman.png)';
+                    cartaVirada.style.backgroundImage = 'url("images/Cuphead_and_his_pal_Mugman.png)';
+                }
+                cartaVirada = null;
+            }
+            else{
+            carta.style.backgroundImage = `url(images/${imagens[Number(carta.id)]})`;
+            cartaVirada = carta;
+            }
+        }
+    }
+}, 3000);
